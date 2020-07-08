f2b9cba8145b6412215d93824d026ccfe3808a7f
Simon <simon@cornet.dev>
2020-07-08 16:49:34 +0200

    add user details inputs

---
+++ src/components/UserDetailsForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>Your details</h2>
+    <v-form>
+      <v-row>
+        <v-col lg="6" sm="12">
+          <v-text-field name="firstname" label="First Name" id="firstname"></v-text-field>
+        </v-col>
+        <v-col lg="6" sm="12">
+          <v-text-field name="lastname" label="Last Name" id="lastname"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6">
+          <v-text-field name="jobTitle" label="Job Title" id="jobTitle"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6">
+          <v-text-field name="email" label="Email" id="email"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-btn color="primary" dark>Submit</v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
-  <card-preview />
+  <v-container fluid>
+    <v-row>
+      <v-col lg="6" sm="12">
+        <card-preview />
+      </v-col>
+      <v-col lg="6" sm="12">
+        <user-details-form></user-details-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import CardPreview from "../components/CardPreview";
+import UserDetailsForm from "../components/UserDetailsForm";
 
 export default {
   components: {
-    CardPreview
+    CardPreview,
+    UserDetailsForm
   }
 };
 </script>
